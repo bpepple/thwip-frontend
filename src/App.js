dbe4ae7dc594a060f24fe4829201d178b77e0e62
Brian Pepple <bdpepple@gmail.com>
2018-07-14 14:56:24 -0400

    Move the Main.js code to App.js.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
-import Main from "./Main";
+import { Switch, Route } from "react-router-dom";
+import PublisherList from "./PublisherList";
+import PublisherDetail from "./PublisherDetail";
+import SeriesList from "./SeriesList";
+import SeriesDetail from "./SeriesDetail";
+import Reader from "./Reader";
 import Header from "./Header";
 import { Container } from "reactstrap";
 
 const App = () => (
   <Container>
     <Header />
-    <Main />
+    <main>
+      <Switch>
+        <Route exact path="/series" component={SeriesList} />
+        <Route path="/series/:slug" component={SeriesDetail} />
+        <Route exact path="/publisher" component={PublisherList} />
+        <Route path="/publisher/:slug" component={PublisherDetail} />
+        <Route
+          path="/reader/:slug/"
+          render={props => <Reader pages="15" {...props} />}
+        />
+      </Switch>
+    </main>
   </Container>
 );
 
