fe23baf548dbdc897610aa98df5af7589818211d
Brian Pepple <bdpepple@gmail.com>
2018-07-23 17:34:42 -0400

    Add a modal window for the Issue cards.
    * For now, I'm just showing the issue's description, but down
      the road I might look at adding more information like
      Creator credits.

--- src/IssueCard.js
+++ src/IssueCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import shortid from 'shortid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -15,50 +15,92 @@ import {
   CardBody,
   CardText,
   Progress,
-  Button
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
 } from 'reactstrap';
 
 const uuid = shortid.generate;
 
-const IssueCard = ({ data }) => (
-  <Container fluid={true}>
-    <Fade in={true}>
-      <Row>
-        {data.map(el => (
-          <Col xs="2" key={uuid()}>
-            <Card className="text-white bg-dark mb-3" key={uuid()}>
-              <CardHeader className="text-center" key={uuid()}>
-                {el.__str__}
-              </CardHeader>
-              <CardImg src={el.image} alt="Placeholder image" key={uuid()} />
-              <Progress value={el.read_percentage} />
-              <CardBody>
-                <CardText key={uuid()}>{el.page_count} pages</CardText>
-              </CardBody>
-              <CardFooter>
-                <Button
-                  color="primary"
-                  href={`/reader/${el.slug}`}
-                  key={uuid()}
-                >
-                  Read
-                </Button>
-                <Button
-                  className="float-right"
-                  color="info"
-                  href="#"
-                  key={uuid()}
-                >
-                  <FontAwesomeIcon icon="info-circle" size="lg" />
-                </Button>
-              </CardFooter>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Fade>
-  </Container>
-);
+class IssueCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { modal: false, desc: '', title: '' };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({ modal: !this.state.modal });
+  }
+
+  open(description, title) {
+    this.setState({
+      modal: true,
+      desc: description,
+      title: title
+    });
+  }
+
+  render() {
+    const { data } = this.props;
+    const { modal, desc, title } = this.state;
+
+    return (
+      <Container fluid={true}>
+        <Modal isOpen={modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>{title} Summary</ModalHeader>
+          <ModalBody>{desc}</ModalBody>
+          <ModalFooter>
+            <Button onClick={this.toggle}>Close</Button>
+          </ModalFooter>
+        </Modal>
+        <Fade in={true}>
+          <Row>
+            {data.map(el => (
+              <Col xs="2" key={uuid()}>
+                <Card className="text-white bg-dark mb-3" key={uuid()}>
+                  <CardHeader className="text-center" key={uuid()}>
+                    {el.__str__}
+                  </CardHeader>
+                  <CardImg
+                    src={el.image}
+                    alt="Placeholder image"
+                    key={uuid()}
+                  />
+                  <Progress value={el.read_percentage} />
+                  <CardBody>
+                    <CardText key={uuid()}>{el.page_count} pages</CardText>
+                  </CardBody>
+                  <CardFooter>
+                    <Button
+                      color="primary"
+                      href={`/reader/${el.slug}`}
+                      key={uuid()}
+                    >
+                      Read
+                    </Button>
+                    <Button
+                      className="float-right"
+                      color="info"
+                      onClick={this.open.bind(this, el.desc, el.__str__)}
+                      key={uuid()}
+                    >
+                      <FontAwesomeIcon icon="info-circle" size="lg" />
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Fade>
+      </Container>
+    );
+  }
+}
 
 IssueCard.propTypes = {
   data: PropTypes.array.isRequired
