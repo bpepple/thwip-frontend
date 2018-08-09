c279c056be19959d26f561c692cdc25f6db799af
Brian Pepple <bdpepple@gmail.com>
2018-08-09 10:01:17 -0400

    Add a TODO comment to the Footer test.

--- src/components/Footer.test.js
+++ src/components/Footer.test.js
@@ -5,3 +5,5 @@ import Footer from './Footer';
 test('renders without crashing', () => {
   shallow(<Footer cvUrl="https://comicvine.gamespot.com/" />);
 });
+
+/* TODO: add test to verify the Comic Vine logo is present. */
