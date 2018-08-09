7e64a31785755adb700824916e67728d69b82cfb
Brian Pepple <bdpepple@gmail.com>
2018-08-09 15:27:21 -0400

    Simple test to see that the App renders.

---
+++ src/components/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+
+test('renders without crashing', () => {
+  shallow(
+    <Router>
+      <App />
+    </Router>
+  );
+});
