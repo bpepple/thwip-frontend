04b0d120d3b6aff274a10646e701c21ef7fbcc88
Brian Pepple <bdpepple@gmail.com>
2018-08-10 11:02:25 -0400

    Add backslash to url endpoint.

--- src/components/SeriesList.js
+++ src/components/SeriesList.js
@@ -3,7 +3,7 @@ import SeriesPage from './SeriesPage';
 
 const SeriesList = props => (
   <SeriesPage
-    endpoint={process.env.REACT_APP_API_URL + '/api/series'}
+    endpoint={process.env.REACT_APP_API_URL + '/api/series/'}
     page={props.match.params.page}
   />
 );
