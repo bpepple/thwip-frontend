5aaae1ceff00cc299ecb61f22fee68309c0e4fbe
Brian Pepple <bdpepple@gmail.com>
2018-08-16 15:07:59 -0400

    Drop the fetchfeature, since it's not being used.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as authReducer } from './auth';
-import { reducer as featureReducer } from './feature';
 import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   form: formReducer,
-  auth: authReducer,
-  features: featureReducer
+  auth: authReducer
 });
 
 export default rootReducer;
