0e911f40d7fbb8fe7ab9942b2471665cfb6e563d
Brian Pepple <bdpepple@gmail.com>
2018-08-28 12:45:41 -0400

    Splite the routes into a separate file.

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ConnectedRouter } from 'connected-react-router';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import PublisherList from './PublisherList';
-import PublisherDetail from './PublisherDetail';
-import SeriesList from './SeriesList';
-import SeriesDetail from './SeriesDetail';
-import RecentIssues from './RecentIssues';
-import Reader from './reader/Reader';
 import MainBar from './MainBar';
+import Routes from './Routes';
 import { Container } from 'reactstrap';
-import { PrivateRoute } from './PrivateRoute';
-
-import Login from './auth/login';
-import Logout from './auth/logout';
-
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faTimesCircle,
@@ -29,34 +18,7 @@ const App = ({ history }) => (
     <ConnectedRouter history={history}>
       <React.Fragment>
         {!history.location.pathname.includes('/reader/') && <MainBar />}
-        <Switch>
-          <PrivateRoute
-            exact
-            path="/series/page/:page"
-            component={SeriesList}
-          />
-          <PrivateRoute
-            path="/series/:slug/page/:page"
-            component={SeriesDetail}
-          />
-          <PrivateRoute
-            exact
-            path="/publisher/page/:page"
-            component={PublisherList}
-          />
-          <PrivateRoute
-            path="/publisher/:slug/page/:page"
-            component={PublisherDetail}
-          />
-          <PrivateRoute
-            path="/issues/recent/page/:page"
-            component={RecentIssues}
-          />
-          <PrivateRoute path="/reader/:slug" component={Reader} />} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/logout" component={Logout} />
-          <Redirect from="/" to="/series/page/1" />
-        </Switch>
+        <Routes />
       </React.Fragment>
     </ConnectedRouter>
   </Container>
