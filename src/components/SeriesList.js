c23be0a49f948e0296e9c3ff3e3dadf7190f0d4f
Brian Pepple <bdpepple@gmail.com>
2018-08-29 10:05:40 -0400

    Rewrite the SeriesList view to fetch with redux.

--- src/components/SeriesList.js
+++ src/components/SeriesList.js
@@ -1,11 +1,58 @@
-import React from 'react';
-import SeriesPage from './SeriesPage';
+import React, { Component } from 'react';
+import * as actions from '../actions';
+import { connect } from 'react-redux';
+import IssueCard from './IssueCard';
+import MainPagination from './MainPagination';
+import Footer from './Footer';
 
-const SeriesList = props => (
-  <SeriesPage
-    endpoint={process.env.REACT_APP_API_URL + '/api/series/'}
-    page={props.match.params.page}
-  />
-);
+class RecentIssues extends Component {
+  constructor(props) {
+    super(props);
 
-export default SeriesList;
+    this.state = {
+      page: this.props.match.params.page
+    };
+  }
+
+  componentDidMount() {
+    this.props.fetchSeriesList(this.state.page);
+  }
+
+  onPageChanged = pageData => {
+    const { currentPage } = pageData;
+
+    /* Don't fetch the page twice. */
+    if (currentPage === Number(this.state.page)) {
+      return;
+    }
+
+    this.props.fetchSeriesList(currentPage);
+    this.setState({ page: currentPage });
+  };
+
+  render() {
+    const { data, loaded } = this.props;
+    const { page } = this.state;
+
+    return loaded ? (
+      <React.Fragment>
+        <IssueCard data={data} />
+        <MainPagination
+          totalRecords={data.count}
+          onPageChanged={this.onPageChanged}
+          page={page}
+        />
+        <Footer cvUrl="https://comicvine.gamespot.com/" />
+      </React.Fragment>
+    ) : null;
+  }
+}
+
+const mapStateToProps = state => {
+  return { data: state.fetch.data, loaded: state.fetch.loaded };
+};
+
+export default connect(
+  mapStateToProps,
+  actions
+)(RecentIssues);
