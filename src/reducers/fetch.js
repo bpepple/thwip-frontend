6468200d4f0dfab93819103548b23319348f03a5
Brian Pepple <bdpepple@gmail.com>
2018-08-29 10:03:39 -0400

    Add some fetch view reducers.

--- src/reducers/fetch.js
+++ src/reducers/fetch.js
@@ -1,10 +1,22 @@
-import { FETCH_RECENT_ISSUES, FETCH_SERIES_DETAIL } from '../actions/types';
+import {
+  FETCH_RECENT_ISSUES,
+  FETCH_SERIES_DETAIL,
+  FETCH_SERIES_LIST,
+  FETCH_PUBLISHER_DETAIL,
+  FETCH_PUBLISHER_LIST
+} from '../actions/types';
 
 export const reducer = (state = {}, action) => {
   switch (action.type) {
     case FETCH_RECENT_ISSUES:
       return { ...state, error: '', loaded: true, data: action.data };
+    case FETCH_SERIES_LIST:
+      return { ...state, error: '', loaded: true, data: action.data };
     case FETCH_SERIES_DETAIL:
+      return { ...state, error: '', loaded: true, data: action.data };
+    case FETCH_PUBLISHER_LIST:
+      return { ...state, error: '', loaded: true, data: action.data };
+    case FETCH_PUBLISHER_DETAIL:
       return { ...state, error: '', loaded: true, data: action.data };
     default:
       return state;
