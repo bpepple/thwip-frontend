7029e3ec38d5491cfc987ce8c63d7c1f75127901
Brian Pepple <bdpepple@gmail.com>
2018-09-06 07:40:35 -0400

    Make the Footer a Pure Component.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import cvLogo from '../img/comicvine_logo.png';
 import { Container, Row } from 'reactstrap';
 
-const Footer = ({ cvUrl }) => (
-  <Container fluid={true}>
-    <Row className="justify-content-end">
-      <h6>
-        Metadata retrieved from{' '}
-        <a href={cvUrl} target="_blank">
-          <img src={cvLogo} alt="Comic Vine" />
-        </a>
-      </h6>
-    </Row>
-  </Container>
-);
+class Footer extends PureComponent {
+  render() {
+    const { cvUrl } = this.props;
+
+    return (
+      <Container fluid={true}>
+        <Row className="justify-content-end">
+          <h6>
+            Metadata retrieved from{' '}
+            <a href={cvUrl} target="_blank">
+              <img src={cvLogo} alt="Comic Vine" />
+            </a>
+          </h6>
+        </Row>
+      </Container>
+    );
+  }
+}
 
 Footer.propTypes = {
   cvUrl: PropTypes.string.isRequired
