b5ae3e54fc3e2828865d8eb4dd30c2868cdf8137
Brian Pepple <bdpepple@gmail.com>
2018-09-06 16:33:02 -0400

    Refactor the RecentIssues to use the fetch actions.

--- src/components/RecentIssues.js
+++ src/components/RecentIssues.js
@@ -1,11 +1,70 @@
-import React from 'react';
-import IssuePage from './IssuePage';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { FETCH_RECENT_ISSUES } from '../actions/types';
+import * as actions from '../actions';
+import { connect } from 'react-redux';
+import IssueCard from './IssueCard';
+import MainPagination from './MainPagination';
+import Footer from './Footer';
 
-const RecentIssues = props => (
-  <IssuePage
-    endpoint={process.env.REACT_APP_API_URL + '/api/issue/recent/'}
-    page={props.match.params.page}
-  />
-);
+class RecentIssues extends Component {
+  constructor(props) {
+    super(props);
 
-export default RecentIssues;
+    this.state = {
+      page: this.props.match.params.page
+    };
+  }
+
+  componentDidMount() {
+    this.props.fetchApiList(FETCH_RECENT_ISSUES, this.state.page);
+  }
+
+  onPageChanged = pageData => {
+    const { currentPage } = pageData;
+
+    /* Don't fetch the page twice. */
+    if (currentPage === Number(this.state.page)) {
+      return;
+    }
+
+    this.props.fetchApiList(FETCH_RECENT_ISSUES, currentPage);
+    this.setState({ page: currentPage });
+  };
+
+  render() {
+    const { data, loaded } = this.props;
+    const { page } = this.state;
+
+    return loaded ? (
+      <React.Fragment>
+        <IssueCard data={data} />
+        <MainPagination
+          totalRecords={data.count}
+          onPageChanged={this.onPageChanged}
+          page={page}
+        />
+        <Footer cvUrl="https://comicvine.gamespot.com/" />
+      </React.Fragment>
+    ) : null;
+  }
+}
+
+RecentIssues.propTypes = {
+  data: PropTypes.object,
+  loaded: PropTypes.bool.isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      page: PropTypes.string.isRequired
+    }).isRequired
+  }).isRequired
+};
+
+const mapStateToProps = state => {
+  return { data: state.fetch.data, loaded: state.fetch.loaded };
+};
+
+export default connect(
+  mapStateToProps,
+  actions
+)(RecentIssues);
