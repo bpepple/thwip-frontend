87cd52b772b3019f95a179137ebf4d7c45cf0c56
Brian Pepple <bdpepple@gmail.com>
2018-09-06 09:20:25 -0400

    Split the Routes into a separate file.

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,8 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import PublisherList from './PublisherList';
-import PublisherDetail from './PublisherDetail';
-import SeriesList from './SeriesList';
-import SeriesDetail from './SeriesDetail';
-import RecentIssues from './RecentIssues';
-import Reader from './reader/Reader';
 import MainBar from './MainBar';
-import { Container } from 'reactstrap';
-import { PrivateRoute } from './PrivateRoute';
-
+import Routes from './Routes';
 import history from './History';
-
-import Login from './auth/login';
-import Logout from './auth/logout';
-
+import { Container } from 'reactstrap';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faTimesCircle,
@@ -27,24 +15,7 @@ library.add(faTimesCircle, faInfoCircle, faSearch);
 const App = () => (
   <Container fluid={true}>
     {!history.location.pathname.includes('/reader/') && <MainBar />}
-    <Switch>
-      <PrivateRoute exact path="/series/page/:page" component={SeriesList} />
-      <PrivateRoute path="/series/:slug/page/:page" component={SeriesDetail} />
-      <PrivateRoute
-        exact
-        path="/publisher/page/:page"
-        component={PublisherList}
-      />
-      <PrivateRoute
-        path="/publisher/:slug/page/:page"
-        component={PublisherDetail}
-      />
-      <PrivateRoute path="/issues/recent/page/:page" component={RecentIssues} />
-      <PrivateRoute path="/reader/:slug" component={Reader} />} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/logout" component={Logout} />
-      <Redirect from="/" to="/series/page/1" />
-    </Switch>
+    <Routes />
   </Container>
 );
 
---
+++ src/components/Routes.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import PublisherList from './PublisherList';
+import PublisherDetail from './PublisherDetail';
+import SeriesList from './SeriesList';
+import SeriesDetail from './SeriesDetail';
+import RecentIssues from './RecentIssues';
+import Reader from './reader/Reader';
+import { PrivateRoute } from './PrivateRoute';
+import Login from './auth/login';
+import Logout from './auth/logout';
+
+const Routes = () => (
+  <Switch>
+    <PrivateRoute exact path="/series/page/:page" component={SeriesList} />
+    <PrivateRoute path="/series/:slug/page/:page" component={SeriesDetail} />
+    <PrivateRoute
+      exact
+      path="/publisher/page/:page"
+      component={PublisherList}
+    />
+    <PrivateRoute
+      path="/publisher/:slug/page/:page"
+      component={PublisherDetail}
+    />
+    <PrivateRoute path="/issues/recent/page/:page" component={RecentIssues} />
+    <PrivateRoute path="/reader/:slug" component={Reader} />} />
+    <Route path="/login" exact component={Login} />
+    <Route path="/logout" component={Logout} />
+    <Redirect from="/" to="/series/page/1" />
+  </Switch>
+);
+
+export default Routes;
