23d174a0943b0fb837ab53f317f1aef0471d5ae7
Brian Pepple <bdpepple@gmail.com>
2018-09-15 17:26:24 -0400

    Add some more tests for the fetch reducer.

--- src/reducers/__tests__/fetch.test.js
+++ src/reducers/__tests__/fetch.test.js
@@ -18,4 +18,45 @@ describe('fetch reducer', () => {
       count: 0
     });
   });
+  it('fetch error', () => {
+    const beforeState = {};
+    const action = { type: FETCH_ERROR, payload: 'failed' };
+    const afterState = reducer(beforeState, action);
+
+    expect(afterState).toEqual({ error: 'failed' });
+  });
+  it('fetch recent issues', () => {
+    const data = {
+      __str__: '52 #001',
+      slug: '52-001-2006',
+      cvurl:
+        'https://comicvine.gamespot.com/52-1-golden-lads-lasses-must/4000-105733/',
+      series: '52',
+      name: 'Golden Lads & Lasses Must...',
+      number: '001',
+      date: '2006-05-10',
+      leaf: 0,
+      page_count: 24,
+      percent_read: 100,
+      status: 2,
+      desc: 'After the INFINITE CRISIS and before One Year Later.',
+      image:
+        'http://127.0.0.1:8000/media/images/issues/6ad10fb8-6106-4d06-b0b4-0aed825e145f.jpg'
+    };
+    const beforeState = {};
+    const action = {
+      type: FETCH_RECENT_ISSUES,
+      page: 2,
+      count: 90,
+      data: data
+    };
+    const afterState = reducer(beforeState, action);
+    expect(afterState).toEqual({
+      error: '',
+      loaded: true,
+      count: 90,
+      page: 2,
+      data: data
+    });
+  });
 });
