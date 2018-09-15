605450746cb55443cc187d79a488c4c271c9f473
Brian Pepple <bdpepple@gmail.com>
2018-09-15 16:41:34 -0400

    Add tests for the auth reducer.

---
+++ src/reducers/__tests__/auth.test.js
@@ -0,0 +1,26 @@
+import { reducer } from '../auth';
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from '../../actions/types';
+
+describe('auth reducer', () => {
+  it('authenticated user', () => {
+    const beforeState = {};
+    const action = { type: AUTH_USER };
+    const afterState = reducer(beforeState, action);
+
+    expect(afterState).toEqual({ error: '', authenticated: true });
+  });
+  it('unauthenticated user', () => {
+    const beforeState = {};
+    const action = { type: UNAUTH_USER };
+    const afterState = reducer(beforeState, action);
+
+    expect(afterState).toEqual({ authenticated: false });
+  });
+  it('authentication error', () => {
+    const beforeState = {};
+    const action = { type: AUTH_ERROR, payload: 'failed' };
+    const afterState = reducer(beforeState, action);
+
+    expect(afterState).toEqual({ error: 'failed' });
+  });
+});
