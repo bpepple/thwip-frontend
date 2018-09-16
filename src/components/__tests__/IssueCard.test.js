333861a38cb588ef3f097302751bb462c9139cb6
Brian Pepple <bdpepple@gmail.com>
2018-09-16 14:33:31 -0400

    Fix the IssueCard test.

--- src/components/__tests__/IssueCard.test.js
+++ src/components/__tests__/IssueCard.test.js
@@ -16,57 +16,134 @@ describe('IssueCard', () => {
   beforeEach(() => {
     props = {
       data: {
-        count: 1,
-        next: null,
-        previous: null,
-        results: [
-          {
-            __str__: 'Action Comics #681',
-            slug: 'action-comics-681-1992',
-            cvurl:
-              'https://comicvine.gamespot.com/action-comics-681-odds-and-endings/4000-116434/',
-            series: 'action-comics-1',
-            name: 'Odds & ...Endings',
-            number: '681',
-            date: '1992-09-30',
-            leaf: 0,
-            page_count: 23,
-            percent_read: 0,
-            status: 0,
-            desc:
-              "S.T.A.R. Labs' Metropolis branch is being rebuilt, bringing about the return of Rampage.",
-            image:
-              'http://127.0.0.1:8000/media/images/issues/4231619f-d71b-47eb-81bc-cca328938e3e.jpg',
-            credits: [
-              {
-                creator: 'Art Thibert',
-                role: [
-                  { name: 'Cover' },
-                  { name: 'Inker' },
-                  { name: 'Penciler' }
-                ]
-              },
-              { creator: 'Bill Oakley', role: [{ name: 'Letterer' }] },
-              { creator: 'Denis Rodier', role: [{ name: 'Inker' }] },
-              {
-                creator: 'Glenn Whitmore',
-                role: [{ name: 'Colorist' }, { name: 'Cover' }]
-              },
-              { creator: 'Jackson Butch Guice', role: [{ name: 'Penciler' }] },
-              { creator: 'Jennifer Frank', role: [{ name: 'Editor' }] },
-              { creator: 'Mike Carlin', role: [{ name: 'Editor' }] },
-              { creator: 'Roger Stern', role: [{ name: 'Writer' }] }
-            ]
+        entities: {
+          credits: {
+            '36698': {
+              id: 36698,
+              creator: 'Aaron Conley',
+              image:
+                'http://127.0.0.1:8000/media/images/creators/74422152-72b1-40a1-87e7-c45c45f77a8c.png',
+              role: [
+                {
+                  id: 4,
+                  name: 'Artist'
+                },
+                {
+                  id: 5,
+                  name: 'Cover'
+                }
+              ]
+            },
+            '36699': {
+              id: 36699,
+              creator: 'Carey Hall',
+              image:
+                'http://127.0.0.1:8000/media/images/creators/db60743b-2b23-4600-9561-a48e47a2eee9.png',
+              role: [
+                {
+                  id: 10,
+                  name: 'Other'
+                }
+              ]
+            },
+            '36700': {
+              id: 36700,
+              creator: 'Jean-Francois Beaulieu',
+              image:
+                'http://127.0.0.1:8000/media/images/creators/9fdbcb87-60a6-4d54-a8b7-522c9667bd66.jpg',
+              role: [
+                {
+                  id: 3,
+                  name: 'Colorist'
+                }
+              ]
+            },
+            '36701': {
+              id: 36701,
+              creator: 'Kent Wagenschutz',
+              image:
+                'http://127.0.0.1:8000/media/images/creators/21900c28-08e7-44d2-87d7-9c0f75eba48c.png',
+              role: [
+                {
+                  id: 6,
+                  name: 'Editor'
+                }
+              ]
+            },
+            '36702': {
+              id: 36702,
+              creator: 'Nate Piekos',
+              image:
+                'http://127.0.0.1:8000/media/images/creators/7c91a581-cc44-4c87-aa15-83542d7f3641.jpg',
+              role: [
+                {
+                  id: 1,
+                  name: 'Letterer'
+                }
+              ]
+            },
+            '36703': {
+              id: 36703,
+              creator: 'Scott O. Brown',
+              image:
+                'http://127.0.0.1:8000/media/images/creators/30004682-48fc-4747-bdc5-0e9b1f62bde2.png',
+              role: [
+                {
+                  id: 5,
+                  name: 'Cover'
+                }
+              ]
+            },
+            '36704': {
+              id: 36704,
+              creator: 'Skottie Young',
+              image:
+                'http://127.0.0.1:8000/media/images/creators/f39a4957-a494-40ab-81fb-e097cb7e5c2e.jpg',
+              role: [
+                {
+                  id: 5,
+                  name: 'Cover'
+                },
+                {
+                  id: 2,
+                  name: 'Writer'
+                }
+              ]
+            }
+          },
+          issues: {
+            '4945': {
+              id: 4945,
+              __str__: 'Bully Wars #001',
+              slug: 'bully-wars-001-2018',
+              cvurl: 'https://comicvine.gamespot.com/bully-wars-1/4000-683718/',
+              series: 'bully-wars',
+              name: 'None',
+              number: '001',
+              date: '2018-09-04',
+              leaf: 0,
+              page_count: 30,
+              percent_read: 0,
+              status: 0,
+              desc:
+                'I HATE FAIRYLAND and Deadpool writer SKOTTIE YOUNG and artist AARON CONLEY (Sabertooth Swordsman, Rocket Raccoon & Groot) team up for a hilarious all-new, all-ages ONGOING SERIES! Rufus, the biggest bully in Rottenville since kindergarten, suddenly goes from bully to bullied on the first day of high school. He’s forced to make a shaky pact with his favorite geeks: Spencer and his twin sibling besties, Edith and Ernie. Together they’ll have to find a way to survive the Hunger Games-like contest ',
+              image:
+                'http://127.0.0.1:8000/media/images/issues/af64e93d-0c1a-4491-ba2f-55f6a65d17e0.jpg',
+              credits: [36698, 36699, 36700, 36701, 36702, 36703, 36704]
+            }
           }
-        ]
-      }
+        },
+        result: [4945]
+      },
+      page: '1',
+      count: 1
     };
     mountedIssueCard = undefined;
   });
 
   describe('rendered `IssueCard`', () => {
     it('contains everything else that gets rendered', () => {
-      const c = issueCard().find('Container');
+      const c = issueCard().find('Row');
       expect(c.length).toBeGreaterThan(0);
     });
     it('always renders an `InfoButton`', () => {
