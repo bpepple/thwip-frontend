4ba87606e19ee94a8b515bc4b47e14e0784f040c
Brian Pepple <bdpepple@gmail.com>
2018-09-22 15:01:52 -0400

    Add the SearchInput component to the MainBar.

--- src/components/MainBar.js
+++ src/components/MainBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { authHeader } from './helpers/auth-header';
+import SearchInput from './SearchInput';
 import {
   Alert,
   Button,
@@ -83,6 +84,7 @@ class MainBar extends Component {
             </NavItem>
           </Nav>
           <Nav className="ml-auto" navbar>
+            <SearchInput />
             {this.renderButton()}
             <NavItem>{this.renderLinks()}</NavItem>
           </Nav>
