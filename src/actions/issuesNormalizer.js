4b411c20adc612d20b440a2ebd99b0859d6dbe82
Brian Pepple <bdpepple@gmail.com>
2018-09-29 10:45:51 -0400

    Normalize the arc info for issues.

--- src/actions/issuesNormalizer.js
+++ src/actions/issuesNormalizer.js
@@ -2,7 +2,11 @@ import { schema, normalize } from 'normalizr';
 
 const roleSchema = new schema.Entity('roles');
 const creditSchema = new schema.Entity('credits', { role: [roleSchema] });
-const issueSchema = new schema.Entity('issues', { credits: [creditSchema] });
+const arcSchema = new schema.Entity('arcs');
+const issueSchema = new schema.Entity('issues', {
+  arcs: [arcSchema],
+  credits: [creditSchema]
+});
 const issueList = [issueSchema];
 
 const issuesNormalizer = data => normalize(data, issueList);
