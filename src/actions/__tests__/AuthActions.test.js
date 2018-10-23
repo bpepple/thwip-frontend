25e29c011a3acb02b398ba99473e1484feda7a7f
Brian Pepple <bdpepple@gmail.com>
2018-10-23 14:46:03 -0400

    Add a test for the logoutUser() action

--- src/actions/__tests__/AuthActions.test.js
+++ src/actions/__tests__/AuthActions.test.js
@@ -2,6 +2,9 @@ import * as actions from '../index.js';
 import * as types from '../types.js';
 
 describe('actions', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
   it('should create an action for an AUTH_ERROR', () => {
     const error = 'Failed to authorize';
     const expectedAction = {
@@ -9,5 +12,18 @@ describe('actions', () => {
       payload: error
     };
     expect(actions.authError(error)).toEqual(expectedAction);
+  });
+  it('should remove token on logoutUser', () => {
+    const VALUE = '12345',
+      KEY = 'token';
+    /* Set a test token value */
+    localStorage.setItem(KEY, VALUE);
+    expect(localStorage.__STORE__[KEY]).toBe(VALUE);
+    expect(localStorage.length).toBe(1);
+
+    /* Let's test that the logoutUser action works */
+    actions.logoutUser();
+    expect(localStorage.__STORE__).toEqual({});
+    expect(localStorage.length).toBe(0);
   });
 });
