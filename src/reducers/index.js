68534c950453883d41d95483077949e5fcdf6fc8
Brian Pepple <bdpepple@gmail.com>
2019-06-22 17:45:58 -0400

    Fix import order.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import { reducer as authReducer } from './auth';
 import { reducer as fetchReducer } from './fetch';
-import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   form: formReducer,
