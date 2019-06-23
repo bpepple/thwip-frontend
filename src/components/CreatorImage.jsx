3df09e3796f6c6f34b4dc9a19722eaae2e96eb1c
Brian Pepple <bdpepple@gmail.com>
2019-06-23 10:44:40 -0400

    Fix some easy lint errors

--- src/components/CreatorImage.jsx
+++ src/components/CreatorImage.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import noImage from '../img/creator-not-found.png';
 import { Media } from 'reactstrap';
+import noImage from '../img/creator-not-found.png';
 
 const CreatorImage = ({ src }) => {
   if (src) {
     return (
       <Media object className="rounded-left" src={src} alt="Creator image" />
     );
-  } else {
-    return (
-      <Media
-        object
-        className="rounded-left"
-        src={noImage}
-        alt="Missing image"
-      />
-    );
   }
+  return (
+    <Media
+      object
+      className="rounded-left"
+      src={noImage}
+      alt="Missing image"
+    />
+  );
+};
+
+CreatorImage.defaultProps = {
+  src: null,
 };
 
 CreatorImage.propTypes = {
-  src: PropTypes.string
+  src: PropTypes.string,
 };
 
 export default CreatorImage;
